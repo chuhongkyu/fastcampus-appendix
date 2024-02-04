cbdb1e071e5fa4b57fe37b6c90df017d05e357eb
Mr.chu <chuhongkyu@gmail.com>
2024-02-04 21:09:53 +0900

    Fixed Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,31 +4,47 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 const webpackMode = process.env.NODE_ENV || 'development';
 
+const entryAndHtmlConfig = [
+  { name: 'example01', htmlTemplate: './src/example01/index.html' },
+  { name: 'example02', htmlTemplate: './src/example02/index.html' },
+  { name: 'example03', htmlTemplate: './src/example03/index.html' },
+  { name: 'example04', htmlTemplate: './src/example04/index.html' },
+  { name: 'example05', htmlTemplate: './src/example05/index.html' },
+  { name: 'example06', htmlTemplate: './src/example06/index.html' },
+];
+
+const pluginHtml = entryAndHtmlConfig.map(config => {
+  return (
+    new HtmlWebpackPlugin({
+      template: config.htmlTemplate,
+      filename: `${config.name}.html`,
+    })
+  )
+});
+
 module.exports = {
   mode: webpackMode,
-  entry: './src/index.js', // 프로젝트 진입점 파일
+  entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'), // 빌드된 파일의 출력 디렉토리
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
   },
   devServer: {
-    port: 8080, // 개발 서버 포트
+    port: 8080,
   },
   plugins: [
+    ...pluginHtml, 
     new HtmlWebpackPlugin({
-      template: './src/index.html', // HTML 템플릿 파일 경로
-      filename: 'index.html', // 생성될 HTML 파일 이름
+      template: './src/index.html',
+      filename: 'index.html',
     }),
     new CopyWebpackPlugin({
-			patterns: [
-				{ from: "./src/assets", to: "assets" },
-        { from: "./src/example01", to: "example01" },
-        { from: "./src/example02", to: "example02" },
-        { from: "./src/example03", to: "example03" },
-        { from: "./src/example04", to: "example04" },
-        { from: "./src/example05", to: "example05" },
-        { from: "./src/example06", to: "example06" },
-			],
-		})
-  ],
+      patterns: [
+        { from: './src/assets', to: 'assets'},
+        { from: './src/css', to: 'css' }
+      ]
+    })
+  ]
 };
+
