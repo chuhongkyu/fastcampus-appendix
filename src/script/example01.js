a8ac22922b282df8723b175476ac5fb998b8e0a8
Mr.chu <chuhongkyu@gmail.com>
2024-02-05 17:04:11 +0900

    Add Scroll Action

--- src/script/example01.js
+++ src/script/example01.js
@@ -1,19 +1,16 @@
-
 export function pageExample01 () {
-  var FRAMES = 148;
-  var FPS = 30;
-  var video = document.getElementById('video');
+  const FRAMES = 148;
+  const FPS = 30;
+  const video = document.getElementById('video');
 
-  window.addEventListener('scroll', function (e) {
-    var time = (window.scrollY / 1000) * FRAMES / FPS;
+  window.addEventListener('scroll', () => {
+    const time = (window.scrollY / 1000) * FRAMES / FPS;
     video.currentTime = time;
     console.log(time);
-    // alert('Hizo scroll')
   });
 
-  window.addEventListener('load', function(e) {
+  window.addEventListener('load', () => {
     video.pause();
     video.currentTime = 0;
   });
 }
-
