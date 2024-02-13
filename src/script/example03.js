f81653292db2918182ddc6de262feb8cc10630e9
Mr.chu <chuhongkyu@gmail.com>
2024-02-13 16:32:52 +0900

    Add example03

--- src/script/example03.js
+++ src/script/example03.js
@@ -1,50 +1,48 @@
 
 export function pageExample03 () {
-const html = document.documentElement;
-const canvas = document.getElementById("scrollAnimation");
-const context = canvas.getContext("2d");
-const copy = document.getElementById("title");
+  const html = document.documentElement;
+  const canvas = document.getElementById("scrollAnimation");
+  const context = canvas.getContext("2d");
+  const copy = document.getElementById("title");
 
-const frameCount = 350;
-const currentFrame = index => (
-  `assets/img/${index}.jpg`
-)
+  const frameCount = 350;
+  const currentFrame = index => (
+    `assets/img/${index}.jpg`
+  )
 
-const img = new Image()
-img.src = currentFrame(1);
-canvas.width= 1280;
-canvas.height=740;
-img.onload=function(){
-  context.drawImage(img, 0, 0);
-}
-
-const updateImage = index => {
-  img.src = currentFrame(index);
-  context.drawImage(img, 0, 0);
-}
-
-window.addEventListener('scroll', () => {  
-  const scrollTop = html.scrollTop;
-  console.log('scrollTop', scrollTop);
-  const maxScrollTop = html.scrollHeight - window.innerHeight
-  console.log('maxScrollTop', maxScrollTop);
-  const scrollFraction = scrollTop / maxScrollTop;
-  
-  copy.style.opacity = scrollFraction > 0.5 ? (1 - scrollFraction) : scrollFraction * 2;
-  const frameIndex = Math.min(
-    frameCount - 1,
-    Math.ceil(scrollFraction * frameCount)
-  );
-  
-  requestAnimationFrame(() => updateImage(frameIndex + 1))
-});
-
-const preloadImages = () => {
-  for (let i = 1; i < frameCount; i++) {
-    const img = new Image();
-    img.src = currentFrame(i);
+  const img = new Image()
+  img.src = currentFrame(1);
+  canvas.width= 1280;
+  canvas.height = 740;
+  img.onload=function(){
+    context.drawImage(img, 0, 0);
   }
-};
 
-preloadImages()
+  const updateImage = index => {
+    img.src = currentFrame(index);
+    context.drawImage(img, 0, 0);
+  }
+
+  window.addEventListener('scroll', () => {  
+    const scrollTop = html.scrollTop;
+    const maxScrollTop = html.scrollHeight - window.innerHeight
+    const scrollFraction = scrollTop / maxScrollTop;
+    
+    copy.style.opacity = scrollFraction > 0.5 ? (1 - scrollFraction) : scrollFraction * 2;
+    const frameIndex = Math.min(
+      frameCount - 1,
+      Math.ceil(scrollFraction * frameCount)
+    );
+    
+    requestAnimationFrame(() => updateImage(frameIndex + 1))
+  });
+
+  const preloadImages = () => {
+    for (let i = 1; i < frameCount; i++) {
+      const img = new Image();
+      img.src = currentFrame(i);
+    }
+  };
+
+  preloadImages()
 }
