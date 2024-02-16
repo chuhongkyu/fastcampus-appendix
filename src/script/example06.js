a603432dec176dec92f0f2c3bdf330cf7a92c6fb
Mr.chu <chuhongkyu@gmail.com>
2024-02-16 14:23:42 +0900

    Add Prod

--- src/script/example06.js
+++ src/script/example06.js
@@ -6,6 +6,7 @@ gsap.registerPlugin(ScrollTrigger);
 export function pageExample06 () {
   makeMainVideo()
   makeSection()
+  makeHeader()
 }
 
 const makeMainVideo = () => {
@@ -74,66 +75,101 @@ const makeMainVideo = () => {
 }
 
 const makeSection = () => {
-    const sections = document.querySelectorAll(".section");
-    const imgs = document.querySelector(".conatiner-img")
-    const Cards = document.querySelectorAll(".card");
+  const sections = document.querySelectorAll(".section");
+  const imgs = document.querySelector(".conatiner-img")
+  const cards = document.querySelectorAll(".card");
 
-    const title1 = document.querySelector(".title1");
-    const title2 = document.querySelector(".title2");
+  const footer = document.querySelector("footer");
+  const footerHeight = footer.clientHeight;
+  console.log(footerHeight)
 
-    const tl = gsap.timeline({
+  const title1 = document.querySelector(".title1");
+  const title2 = document.querySelector(".title2");
+  const tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: sections[1], 
+        pin: true,
+        scrub: 0.3,
+        start: "top top",
+        end: "+=300%",
+        // markers: true,
+        toggleActions: "play reverse play reverse"
+    }
+  });
+  tl.from(title1, {
+      x: '-300%',
+      autoAlpha: 0,
+      duration: 2,
+      ease: "none"
+  });
+
+  tl.from(title2, {
+      x: '300%',
+      autoAlpha: 0,
+      duration: 2,
+      ease: "none"
+  }, "<"); 
+
+  gsap.set(imgs, { opacity: 0, scale: 0});
+  gsap.to(imgs, {
+      opacity: 1,
+      scale: 1,
+      stagger: 0.04,
+      transformOrigin: "center center",
       scrollTrigger: {
-          trigger: sections[1], 
+          trigger: sections[2], 
+          start: "top top",
+          // markers: true,
+          pin: true,
+          toggleActions: "play reverse play reverse"
+      },
+      ease: "elastic.inOut(1.2, 0.75)",
+  });
+  
+  const t3 = gsap.timeline({
+      scrollTrigger: {
+          trigger: sections[3],
           pin: true,
           scrub: 0.3,
           start: "top top",
           end: "+=300%",
-          // markers: true,
-          toggleActions: "play reverse play reverse"
       }
-    });
+  });
+  t3.from(cards, {x: '300%', autoAlpha: 0, duration:2, ease: "none", stagger:3})
+  .to(cards, {duration: 3})
 
-    tl.from(title1, {
-        x: '-300%',
-        autoAlpha: 0,
-        duration: 2,
-        ease: "none"
-    });
-  
-    tl.from(title2, {
-        x: '300%',
-        autoAlpha: 0,
-        duration: 2,
-        ease: "none"
-    }, "<"); 
-
-    gsap.set(imgs, { opacity: 0, scale: 0});
-
-    gsap.to(imgs, {
-        opacity: 1,
-        scale: 1,
-        stagger: 0.04,
-        transformOrigin: "center center",
-        scrollTrigger: {
-            trigger: sections[2], 
-            start: "top top",
-            // markers: true,
-            pin: true,
-            toggleActions: "play reverse play reverse"
-        },
-        ease: "elastic.inOut(1.2, 0.75)",
-    });
-
-    
-    const t3 = gsap.timeline({
-        scrollTrigger: {
-            trigger: sections[3],
-            pin: true,
-            scrub: 0.3,
-            start: "top top",
-            end: "+=300%",
-        }
-    });
-    t3.from(Cards, {x: '300%', autoAlpha: 0, duration:2, ease: "none", stagger:3})
-    .to(Cards, {duration: 3});
+  gsap.set(footer, { height: 0 })
+  const t4 = gsap.timeline({
+    scrollTrigger: {
+        trigger: sections[4],
+        start: "bottom bottom",
+        end: "bottom bottom",
+        // markers: true
+      }
+  });
+  t4.from(footer, { height: 0 })
+  .to(footer, { duration: 1, y: -100, height: footerHeight, ease: "power4.out" })
 }
+
+const makeHeader = () => {
+  const body = document.querySelector("body");
+  let lastScroll = 0;
+
+  window.addEventListener("scroll", () => {
+    const currentScroll = window.pageYOffset;
+
+    if (currentScroll <= 0) {
+      body.classList.remove("up");
+      return;
+    }
+    
+    if (currentScroll > lastScroll && !body.classList.contains("down")) {
+      body.classList.remove("up");
+      body.classList.add("down");
+    } else if (currentScroll < lastScroll && body.classList.contains("down")) {
+      body.classList.remove("down");
+      body.classList.add("up");
+    }
+    lastScroll = currentScroll;
+  });
+};
