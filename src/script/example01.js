7d177feac390824f8768e7cca7229a28d0f01576
Mr.chu <chuhongkyu@gmail.com>
2024-02-20 16:47:47 +0900

    Add example01

--- src/script/example01.js
+++ src/script/example01.js
@@ -1,17 +1,5 @@
 function pageExample01 () {
-  const FRAMES = 150;
-  const FPS = 25;
-  const video = document.getElementById('video');
 
-  window.addEventListener('scroll', () => {
-    const time = (window.scrollY / 1000) * FRAMES / FPS
-    video.currentTime = time;
-  });
-
-  window.addEventListener('load', () => {
-    video.pause();
-    video.currentTime = 0;
-  });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
