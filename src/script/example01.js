dae8df4f64fd5c0db10fc4e0be8e48eeb8cc6d6e
Mr.chu <chuhongkyu@gmail.com>
2024-02-21 15:19:34 +0900

    Add example01

--- src/script/example01.js
+++ src/script/example01.js
@@ -1,16 +1,5 @@
 function pageExample01 () {
-  const video = document.getElementById('video');
-
-  window.addEventListener('scroll', () => {
-    const time = (window.scrollY / 1000) * 5
-    video.currentTime = time;
-    // console.log(time)
-  });
-
-  window.addEventListener('load', () => {
-    video.pause();
-    video.currentTime = 0;
-  });
+  
 }
 
 document.addEventListener('DOMContentLoaded', () => {
